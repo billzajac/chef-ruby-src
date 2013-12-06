fe6f2fb4c20e394f206cfe5f74ad7cce7fd2f9d1
William Zajac <william.zajac@disney.com>
2013-12-06 13:13:53 -0800

    default 2.0.0-p353

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['ruby']['version'] = '2.0.0-p247'
+default['ruby']['version'] = '2.0.0-p353'
 default['ruby']['checksum'] = '3e71042872c77726409460e8647a2f304083a15ae0defe90d8000a69917e20d3'
 
 default[:ruby][:prefix]         = "/usr/local"
--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name              "ruby-src"
-maintainer        "https://www.statuspage.io"
-maintainer_email  "scott@statuspage.io"
+maintainer        "https://www.billzajac.com"
+maintainer_email  "spam@billzajac.com"
 license           "Apache 2.0"
 description       "Installs system ruby from source"
-version           "1.0.0"
+version           "1.0.1"
 recipe            "ruby-src", "Installs system ruby from source."
 
 depends           "build-essential"
